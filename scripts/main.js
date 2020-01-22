e300ea68881e72b571a63f9d35a3e733c6253d3a
Lucas Marchesoti Franco <lucas.marchesoti@callink.com.br>
2020-01-22 12:14:42 -0300

    Show value on slider

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,24 @@
 (function (window) {
   'use strict';
   var FORM_SELECTOR = '[data-coffee-order="form"]';
+  var SLIDER_SELECTOR = '[strength-level="slider"]';
+  var SLIDER_DISPLAY_VALUE_SELECTOR = '[strength-level="display-value"]';
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
   var FormHandler = App.FormHandler;
+  var SliderHandler = App.SliderHandler;
   var myTruck = new Truck('ncc-1701', new DataStore());
   window.myTruck = myTruck;
   var formHandler = new FormHandler(FORM_SELECTOR);
+  var sliderHandler = new SliderHandler(SLIDER_SELECTOR);
 
   formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
   console.log(formHandler);
+
+  sliderHandler.addChangeHandler(function (value) {
+    console.log('Caffeine rating changed from ' + this.innerText + ' to ' + value);
+    this.innerText = value;
+  }.bind(document.querySelector(SLIDER_DISPLAY_VALUE_SELECTOR)));
+  console.log(sliderHandler);
 })(window);
---
+++ scripts/sliderhandler.js
@@ -0,0 +1,29 @@
+(function (window) {
+    'use strict';
+    var App = window.App || {};
+    var $ = window.jQuery;
+
+    function SliderHandler(selector) {
+        if (!selector) {
+            throw new Error('No selector provided');
+        }
+
+        this.$sliderElement = $(selector);
+        if (this.$sliderElement.length === 0) {
+            throw new Error('Could not find element with selector: ' + selector);
+        }
+    }
+
+    SliderHandler.prototype.addChangeHandler = function (fn) {
+        console.log('Setting change handler for slider');
+        this.$sliderElement.on('change', function (event) {
+            event.preventDefault();
+            fn(this.$sliderElement[0].value);
+        }.bind(this));
+        console.log('Presetting values');
+        fn(this.$sliderElement[0].value);
+    };
+
+    App.SliderHandler = SliderHandler;
+    window.App = App;
+})(window);
