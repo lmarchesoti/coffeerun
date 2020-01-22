a215487c4ce6e857aa73c8fae4076ef889dc5ed0
Lucas Marchesoti Franco <lucas.marchesoti@callink.com.br>
2020-01-22 10:49:00 -0300

    Chapter 10

--- scripts/truck.js
+++ scripts/truck.js
@@ -10,12 +10,12 @@
   Truck.prototype.createOrder = function (order) {
     console.log('Adding order for ' + order.emailAddress);
     this.db.add(order.emailAddress, order);
-  }
+  };
 
   Truck.prototype.deliverOrder = function (customerId) {
     console.log('Delivering order for ' + customerId);
     this.db.remove(customerId);
-  }
+  };
 
   Truck.prototype.printOrders = function () {
     var customerIdArray = Object.keys(this.db.getAll());
